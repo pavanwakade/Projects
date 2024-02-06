40bce8676b8cab82678e1a21d55affac9d88f417
Pavan Wakade <130147767+pavanwakade@users.noreply.github.com>
2024-02-06 20:17:29 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+
+
+    function appendToDisplay(value) {
+        document.getElementById('display').value += value;
+    }
+
+    function clearDisplay() {
+        document.getElementById('display').value = '';
+    }
+
+    function calculateResult() {
+        try {
+            document.getElementById('display').value = eval(document.getElementById('display').value);
+        } catch (error) {
+            document.getElementById('display').value = 'Error';
+        }
+    }
